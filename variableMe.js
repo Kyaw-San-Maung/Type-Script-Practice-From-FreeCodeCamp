aa555a02138d849c07a2255c0578b1ff56e3f2dd
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-15 10:31:56 +0630

    Updating js file from the ts file.

--- variableMe.js
+++ variableMe.js
@@ -1,2 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greeting = "Hello World";
 console.log(greeting);
+// number
+var userId = 3222.2;
+userId.toFixed();
+// boolean
+var isLogin = true;
--- variableMe.ts
+++ variableMe.ts
@@ -2,6 +2,14 @@ let greeting: string = "Hello World";
 
 console.log(greeting);
 
+// number
 
+let userId = 3222.2;
+
+userId.toFixed();
+
+// boolean
+
+let isLogin = true;
 
 export {}
