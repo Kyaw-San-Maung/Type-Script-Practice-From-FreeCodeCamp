373ae3ec8fd4cd1b7a28f391fbe9020ec892ce2f
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-17 11:27:54 +0630

    Practicing Unions typeScript type

---
+++ myUnion.ts
@@ -0,0 +1,31 @@
+let studentScore: number | string;
+
+studentScore = 22;
+studentScore = "22"
+
+type user = {
+    name: string,
+    id: number
+}
+
+type admin = {
+    adminName: string,
+    id: number
+}
+
+let kyaw: user | admin = { name: "Uesr", id: 225 }
+
+kyaw = { adminName: "Admin", id: 255 }
+
+function getDbId(id: number | string) {
+    if (typeof id === "string") {
+        id.toLowerCase();
+    } else {
+        
+        id + 2
+    }
+
+}
+
+getDbId(2);
+getDbId("2")
