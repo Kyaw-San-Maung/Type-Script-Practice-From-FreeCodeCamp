273030ee788d24b5c76bccc2244652dccf123d75
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-17 11:03:33 +0630

    Practicing about the Array typeScript.

---
+++ myArray.ts
@@ -0,0 +1,24 @@
+// const superHero:String[] = [];
+
+const superHero: Array<string> = [];
+const superPower: Array<number> = [];
+
+superHero.push("SpiderMan");
+superPower.push(2)
+
+//pushing object to the array  according to the type
+type user = {
+    name: string,
+    isActive: boolean
+}
+
+const allUser: user[] = []
+
+allUser.push({ name: "", isActive: true })
+
+//Returning array to arry
+const MLModel: number[][] = [
+    [225, 225, 225],
+    []
+]
+
