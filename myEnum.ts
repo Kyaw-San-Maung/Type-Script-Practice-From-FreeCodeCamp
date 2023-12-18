fb579d1f0c51417379ac4c6258dc5a9c15ff4579
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-18 15:26:13 +0630

    Practicing about the enum in typescript

---
+++ myEnum.ts
@@ -0,0 +1,15 @@
+// Not Good Way
+// const ASLE = 0;
+// const MIDDLE = 1;
+// const WINDOW = 2;
+
+// let kyawSeat = ASLE
+
+enum SeatChoice {
+    AISLE = "aisle",
+    MIDDLE = 3,
+    WINDOW,
+    FOURTH
+}
+
+const kyawSeat = SeatChoice.AISLE
