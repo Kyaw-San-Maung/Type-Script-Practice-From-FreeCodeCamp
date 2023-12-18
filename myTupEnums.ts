dab3cf9d3fa20fdf0a11c8c3f5093b03023e1ef8
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-18 15:07:10 +0630

    Practicing about the Tuple

---
+++ myTupEnums.ts
@@ -0,0 +1,13 @@
+//Practicing Tuple
+
+let tuser: [string, number, boolean]
+
+tuser = ["Kyaw San Maung", 132, true];
+
+type user = [string, number, boolean]
+
+const newUser: user = ["Kyaw San Maung", 122, true]
+
+//weak point
+
+newUser.push("Kyaw Kyaw")
